1e06d5ad6aecb78225a5ad1024d780166a9e1cdf
FayezKassar <fayez@sirenassociates.com>
2021-01-03 15:28:04 +0200

    Added search box

--- src/app/find-students/find-students.component.ts
+++ src/app/find-students/find-students.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { map } from 'rxjs/operators';
 import { course } from '../find-teacher/course.model';
 
@@ -10,9 +11,11 @@ import { course } from '../find-teacher/course.model';
 })
 export class FindStudentsComponent implements OnInit {
 
+  form: FormGroup;  
   userUrl = 'https://learningisfun-328b1-default-rtdb.firebaseio.com/courses.json'
 
   courses : course[] = [];
+  courses2 : course[] = [];
 
   constructor(private http: HttpClient) { }
 
@@ -23,7 +26,15 @@ export class FindStudentsComponent implements OnInit {
       }
     })).subscribe(res =>
       console.log(res))
+
+    this.courses2 =this.courses
     
-  }
+    this.form = new FormGroup({
+      searchText: new FormControl(null),
+    });
+    this.form.get('searchText').valueChanges.subscribe(x => {
+      this.courses2 = this.courses.filter(y => y.firstName.toLowerCase().startsWith(x) || y.lastName.toLowerCase().startsWith(x) || y.course.toLowerCase().startsWith(x)  )
+    })
+  } 
 
 }
